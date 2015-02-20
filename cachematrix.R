0f2bddbf71ea7a1e51b3c1f77f28437101e8cf10
pithegreat <pithegreat@users.noreply.github.com>
2015-02-20 16:57:48 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The overall purpose is to store the inverse of a function, and if it
+## is not calulated then to do it to cave time
 
-## Write a short comment describing this function
+## This function retrieves and create variables that creates a cached matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) { #sets value of matrix
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x #retrieves the value of the matrix
+  setinverse <- function(inverse) m <<- inverse  #sets the inverse of the matrix
+  getinverse <- function() m #retreives that inverse of the matrix
+  list(set = set, get = get,  #returns the values of the previous functions in a list
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function sees if a inverse has already been solved, and if not solves one
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse() #gets inverse value
+  if(!is.null(m)) { # if inverse value is already there then returns it
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get() #otherwise it gets value of x
+  m <- solve(data, ...) #then it finds the inverse of x
+  x$setinverse(m) #then it sets that inverse to m
+  m #finally it returns the inverse of x
 }
+
